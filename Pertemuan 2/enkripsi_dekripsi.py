96668379a75859650fac66fd9a7d464f1dd148a6
Ezra Muhammad Hilal <91597565+EMH1802@users.noreply.github.com>
2021-10-02 09:37:50 +0700

    Add files via upload

---
+++ Pertemuan 2/enkripsi_dekripsi.py
@@ -0,0 +1,73 @@
+print("===================")
+print("Ezra Muhammad Hilal")
+print("    20190801389    ")
+print("===================")
+
+abjad = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def enkripsi(abjad):
+    str = input("Teks : ")
+    key = int(input("Key : "))
+
+    str = str.lower()
+    result = ''
+
+    for char in str:
+        if char in abjad:
+            n = abjad.index(char)
+            encrypt = (n - key) % 26
+            convert = abjad[encrypt]
+            result = result + convert
+        else:
+            result = result + ''
+
+    print(f"Result : {result}")
+
+
+def dekripsi(abjad):
+    str = input("Teks : ")
+    key = int(input("Key : "))
+
+    str = str.lower()
+    result = ''
+
+    for char in str:
+        if char in abjad:
+            n = abjad.index(char)
+            dencrypt = (n + key) % 26
+            convert = abjad[dencrypt]
+            result = result + convert
+        else:
+            result = result + ''
+
+    print(f"Result : {result}")
+
+
+pilihan = 'y'
+
+while (pilihan == 'y'):
+    print("Menu Pilihan : ")
+    print("1. Enkripsi")
+    print("2. Dekripsi")
+    print("3. Keluar")
+
+    menu = input("Menu Yang Di Pilih : ")
+    print("-------------------------------------")
+
+    if menu == '1':
+        print("Menu Enkripsi Data")
+        enkripsi(abjad)
+    elif menu == '2':
+        print("Menu Dekripsi Data")
+        dekripsi(abjad)
+    elif menu == '3':
+        print("Keluar Dari Program")
+        break
+    else:
+        print("Menu Tidak Ditemukan")
+
+    print("----------------------------------------")
+    pilihan = input("Apakah Ingin Melanjutkan ? (y/n) ")
+    print("----------------------------------------")
